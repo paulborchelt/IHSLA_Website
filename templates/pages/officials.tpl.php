0407599adea158eb4a7ecc7d0bc0ef3963722bf9
Paul Borchelt <paul.borchelt@angieslist.com>
2016-11-08 22:06:24 -0500

    Arbiter Updates

--- templates/pages/officials.tpl.php
+++ templates/pages/officials.tpl.php
@@ -5,4 +5,23 @@
 <p>The Indiana High School Lacrosse Association subcontracts the officials for it's games using the <a href="http://www.InLaxRef.org">Indiana Lacrosse Officials Association (ILOA)</a>.  They use the Arbiter Sports website to schedule all games. Each team is required to have an Arbiter Sports account and should double check the Arbiter Sports site before your game to assure officials have been assigned to it. 
    Please visit their web site <a href="http://www.InLaxRef.org"> www.InLaxRef.org</a> for more information about how to contact officials, official fees, cancelation polices, 
    and how to get an Arbiter Sports account.</p>
+   
+<h3>Arbiter Information</h3>
+<p> 
+<ul>
+    <li>Go to <a href="http://www.arbitersports.com/">http://www.arbitersports.com/</a></li>
+    <li>Please only load the home schedule.</li>
+    <li>Please let Clint Carter <a href="mailto:indylaxassigner@gmail.com">indylaxassigner@gmail.com</a> know if you need teams, sites, or other info added.</li>
+    <li>Please let Clint Carter <a href="mailto:indylaxassigner@gmail.com">indylaxassigner@gmail.com</a> know if another individual associated with your team should have a login.</li>
+</ul>
+<h3>How to Enter Schedule into Arbiter</h3>
+<ol>
+    <li>Click schedule</li>
+    <li>Click the blue number under "Games" for the appropriate team.</li>
+    <li>Click the green + at the top left.</li>
+    <li>Use the various pulldown menus to fill in the appropriate info.  (Note that all teams and sites have the city listed first.  Ex.  Indianapolis Park Tudor, Mishawaka Marian, Evansville Central, etc.  Please let me know if you need something added to the lists.)</li>
+    <li>Click save.</li>
+    <li>Repeat until home schedule is full.</li>
+</ol> 
+</p>
 
