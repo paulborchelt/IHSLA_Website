cfc22ba5cceb284c4668a19c1990c64c59b3df69
Paul Borchelt <paul.borchelt@angieslist.com>
2017-03-28 22:58:05 -0400

    Games Day Card with Just Games
    This is a version of Game Day cards with just games. showing

--- templates/GamesTodayCard.tpl.php
+++ templates/GamesTodayCard.tpl.php
@@ -1,9 +1,13 @@
-<?php while ( $schedule = $result->fetchNextObject() ): ?>
-   	<a><?php echo$schedule->_HomeTeamObject->Team_Name?></a>
-   	<a><?php echo$schedule->_AwayTeamObject->Team_Name?></a>
-   	<a><?php echo$schedule->_SiteObject->field_name?></a>
-   	<a><?php echo$schedule->_TimeObject->getTime()?></a>
-   	<a><?php echo$schedule->Game_Level?></a>
-      <a><?php echo$schedule->getResults(TRUE)?></a>
-      </br>
-<?php endwhile; ?>
+<div class="row-fluid">
+    <div class="span4">
+    <h5>Varsity Games Today:</h5>
+    <p>
+        <small>
+        <?php while ( $schedule = $result->fetchNextObject() ): ?>
+           	<a><?php echo$schedule->_HomeTeamObject->Team_Name?> vs <?php echo$schedule->_AwayTeamObject->Team_Name?> <?php echo$schedule->_TimeObject->getTime()?></a>
+            </br>
+        <?php endwhile; ?>
+        </small>
+    </p>
+    </div>
+</div> 
