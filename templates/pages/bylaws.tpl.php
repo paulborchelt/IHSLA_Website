1f0ae399d2ebed222fb12950aa0fb096c0a37a28
Paul Borchelt <paul.borchelt@angieslist.com>
2018-01-25 23:03:03 -0500

    New 2018 Rules

--- templates/pages/bylaws.tpl.php
+++ templates/pages/bylaws.tpl.php
@@ -3,7 +3,7 @@
 <h3>By-Laws/Docs</h3>
 <ul>
    <li> <a href="leagueuploads/Bylaws.pdf">By-Laws</a> </li>
-   <li> <a href="leagueuploads/RulesPolicies.pdf">Policies and Rules for 2015 Season</a> </li>
+   <li> <a href="leagueuploads/RulesPolicies.pdf">Policies and Rules</a> </li>
    <li> <a href="leagueuploads/Coaches_Code_of_Conduct.pdf">Coaches Code of Conduct</a></li>
    <li> <a href="leagueuploads/RFP.doc">IHSLA State Championship RFP (Word Doc)</a> </li>
    <li> <a href="leagueuploads/RFP.pdf">IHSLA State Championship RFP (PDF)</a> </li>
