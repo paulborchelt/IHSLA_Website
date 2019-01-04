6b66f0ca4c3da74a0605760d8a1ed34b26667dd2
Paul Borchelt <pborchelt@gmail.com>
2019-01-03 23:03:43 -0500

    Made Changes for new board memebers

--- templates/pages/leageinfo.tpl.php
+++ templates/pages/leageinfo.tpl.php
@@ -2,16 +2,16 @@
 <?php echo$leagueinfonavbar ?>
 
 <h3>Board of Directors</h3>
-<p>Mike Williams    '17 - '18   <a href="mailto:michael.a.williams@me.com"><span class="c8">michael.a.williams@me.com</a></p>
-<p>Paul Borchelt    '17 - '18   <a href="mailto:pborchelt@gmail.com"><span class="c8">pborchelt@gmail.com</a></p>
-<p>John Belote      '17 - '18   <a href="mailto:belotejohn@gmail.com"><span class="c8">belotejohn@gmail.com</a></p>
+<p>Mike Williams    '19 - '20   <a href="mailto:michael.a.williams@me.com"><span class="c8">michael.a.williams@me.com</a></p>
+<p>Matt McInrue     '19 - '20   <a href="mailto:vicepresident@ihsla.com"><span class="c8">vicepresident@ihsla.com</a></p>
+<p>John Belote      '19 - '20   <a href="mailto:belotejohn@gmail.com"><span class="c8">belotejohn@gmail.com</a></p>
 <p>Ted Hampton      '18 - '19   <a href="mailto:thampton@brebeuf.org "><span class="c8">thampton@brebeuf.org</a></p>
 <p>Tom Coons        '18 - '19   <a href="mailto:Countonmelacrosse@gmail.com"><span class="c8">Countonmelacrosse@gmail.com</a></p>
 <p>Tim Clark        '18 - '19   <a href="mailto:tkralc@yahoo.com"><span class="c8">tkralc@yahoo.com</a></p>
 <p>Danny Gray       '18 - '19'  <a href="mailto:sportandfitness@yahoo.com"><span class="c8">sportandfitness@yahoo.com</a></p>
 <br />
 <h3>Executive Committee:</h3>
-<p>Paul Borchelt    President   <a href="mailto:president@ihsla.com"><span class="c8">president@ihsla.com</a></p>
-<p>Matt McInrue     Vice President    <a href="mailto:vicepresident@ihsla.com"><span class="c8">vicepresident@ihsla.com</a></p>
-<p>Alex Gregg       Secretary        <a href="mailto:secretary@ihsla.com"><span class="c8">secretary@ihsla.com</a></p>
+<p>Matt McInrue     President   <a href="mailto:president@ihsla.com"><span class="c8">president@ihsla.com</a></p>
+<p>Kevin Bakel      Vice President    <a href="mailto:vicepresident@ihsla.com"><span class="c8">vicepresident@ihsla.com</a></p>
+<p>Jack Meachum     Secretary        <a href="mailto:secretary@ihsla.com"><span class="c8">secretary@ihsla.com</a></p>
 <p>Jay Reeder       Treasurer   <a href="mailto:treasurer@ihsla.com"><span class="c8">treasurer@ihsla.com</a></p>
